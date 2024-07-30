4d7c265d52cb529086b4fe306e7f491aaa748123
Ayberk <ayberkcetinkaya54@gmail.com>
2024-07-30 09:59:41 +0300

    1

--- wit-sprint6-dersnotlari/src/dersNotlari/Gun_1.jsx
+++ wit-sprint6-dersnotlari/src/dersNotlari/Gun_1.jsx
@@ -32,9 +32,62 @@
     
 */
 
+// eslint-disable-next-line react-refresh/only-export-components
 function Title() {
   return <h1>Hi, React!</h1>;
 }
 const container = document.getElementById("app");
-const root = ReactDom.createRoot(container);
+// eslint-disable-next-line no-undef
+const root = ReactDOM.createRoot(container);
 root.render(<Title />);
+
+//Componentların ilk harfi büyük yazılır.
+//render edilirken html elemanı gibi <> arasına yazılır.
+//componenlar ;
+/*
+jsx return eden fonksiyonlardır.
+kendi fonkrsiyonları olabilir
+reactın özel fonksiyonlarını kullanabilir.
+
+
+*/
+
+/*
+JSX içinde JS kullanacak olursak ; 
+{} ile return edilen kodun içinde jhs dünyasına bir pencere açık js yazılabilir. 
+fakat return içinde olduğundan ,kısa ve return eden şeyler yazılabilir.
+Ternary if, 
+&&,|| gibi operatörler kullanılabilir. 
+
+*/
+
+/*
+jsx kuralları:
+tek parent
+her tag kapanmalı
+class yerine className kullanıyoruz
+
+
+*/
+
+/*
+onClick ile nasıl çalışılır ? 
+
+*/
+
+const fonksiyon () =>{
+
+  console.log("merhaba")
+}
+<button onClick={fonksiyon}></button>
+
+//state = bir component ın hafızasıdır.
+// değiştiriken direk üstüne yazmayız ayrıca bir setter kullanırız.
+
+//useState hookunu kullanacaz. : 
+const [ begeni, setBegeni ] = React.useState( 10 );
+
+/*
+react hoook : bir componentın hafızasına ve lifecycle ına bağlı olarak çalışan metodlardır.
+
+*/
